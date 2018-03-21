6789b62cd171b168cb5b8d3d346aeec1f2c862cd
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-03-21 14:02:09 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Utilise </warn> <utilisateur>")
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu ne peut pas warn cette personne !");
   let reason = args.join(" ").slice(22);
 
   if(!warns[wUser.id]) warns[wUser.id] = {
