ec5a5d9ab86f9211610f94fc08d77a0c4b23fb22
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-05-09 03:12:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setPresence({ game: { name: '/commande ', type: 0} });
+  bot.user.setPresence({ game: { name: '[FR] Type /commande ', type: 0} });
 
 });
 
