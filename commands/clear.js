c454544042d3f8bb2cce3d210fcb11cf0184e85d
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-05-09 03:26:38 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("oof.");
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    message.channel.send(`J'ai supprimés ${args[0]} messages.`).then(msg => msg.delete(5000));
   });
 }
 
