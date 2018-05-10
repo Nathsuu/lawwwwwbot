470e0dc863b2823e6c6dd9d04757b626c5bcb99f
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-05-11 00:08:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+cbot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setActivity("/help | 53 server", {type: "WATCHING"});
+
+  //bot.user.setGame("on SourceCade!");
 });
 
 bot.on("message", async message => {
