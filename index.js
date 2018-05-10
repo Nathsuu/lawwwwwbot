3af279ed4ff482ef03a4e530e6a12c5e2cac5776
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-05-11 00:16:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+  bot.user.setActivity(`Serving ${client.guilds.size} servers`, {type: "WATCHING"});
 
 });
 
