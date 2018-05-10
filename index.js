b0ae1b16a7b07a4a41aa1a9e4d7934ccf60d51f9
Lawzenn <35649900+Lawzenn@users.noreply.github.com>
2018-05-10 23:57:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-bot.on("ready", async () => {
-
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setPresence({ game: { name: '[FR] Type /commande ', type: 0} });
-
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
-
 
 bot.on("message", async message => {
 
