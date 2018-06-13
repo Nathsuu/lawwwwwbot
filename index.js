6e0d1ae7b8d8e7a059dd62c92933ea66dcdd2663
Nathsuu <35649900+Nathsuu@users.noreply.github.com>
2018-06-13 21:38:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setGame('s!help', 'https://www.twitch.tv/gotaga');
+  bot.user.setGame('Playing on ' + bot.guilds.size.toString() + ' servers!');
 });
 
 bot.on("message", async message => {
