5629304824f361f503de6e48922a4e575d1a9b87
Nathsuu <35649900+Nathsuu@users.noreply.github.com>
2018-06-17 18:11:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.commands = new Discord.Collection();
 
 bot.on('ready', () => {
   setInterval(function(){
-    guilds = ["Vaction | v!help", "Vaction | v!help | French Bot", "Vaction | by WinDino#3781", `Vaction | ${client.guilds.size} Serveurs`, "Vaction | v!help", `Vaction | ${client.users.size} Utilisateurs`]
+    guilds = ["Vaction | v!help", "Vaction | v!help | French Bot", "Vaction | by WinDino#3781", `Vaction | ${bot.guilds.size} Serveurs`, "Vaction | v!help", `Vaction | ${bot.users.size} Utilisateurs`]
     lecture = Math.floor((Math.random() * guilds.length));
     client.user.setPresence({
       game:{
