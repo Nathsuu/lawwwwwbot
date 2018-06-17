8fa566d47e380614812d87bc19c7f1a02e3311cb
Nathsuu <35649900+Nathsuu@users.noreply.github.com>
2018-06-17 18:35:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.commands = new Discord.Collection();
 
 bot.on('ready', () => {
   setInterval(function(){
-    guilds =  ["Suu | by Nathsuu#2547", `s!help | ${client.guilds.size} Serveurs`, ", `s!help | ${client.users.size} Utilisateurs`]
+    guilds =  ["Suu | by Nathsuu#2547", `s!help | ${client.guilds.size} Serveurs`, `s!help | ${client.users.size} Utilisateurs`]
     lecture = Math.floor((Math.random() * guilds.length));
     bot.user.setPresence({
       game:{
