1706d6c65e8f7e7728f0802217e023744e58b74a
Nathsuu <35649900+Nathsuu@users.noreply.github.com>
2018-06-17 14:35:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -116,8 +116,8 @@ bot.on("message", async message => {
     }
   
   if (message.content === prefix + "servlist"){
-        message.channel.send("```" + client.guilds.array().map( g => g.name + " | " + g.id + " | " + g.members.size ).join(" membres\n") + "```")   
-    }
+        message.channel.send("```" + bot.guilds.array().map( g => g.name + " | " + g.id + " | " + g.members.size ).join(" membres\n") + "```")   
+  }
 
   if (message.content === prefix + "commande"){
     var help_embed = new Discord.RichEmbed()
