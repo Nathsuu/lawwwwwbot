7d5066ca34b4cb79dfbbeb66a312422b3acf559d
Nathsuu <35649900+Nathsuu@users.noreply.github.com>
2018-06-17 14:44:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -131,6 +131,47 @@ bot.on("message", async message => {
     
   }
   
+if (message.content.startsWith(prefix + "hack")) {	
+	if(!message.member.hasPermission("ADMINISTRATOR")) {	
+            return message.reply("Vous n'avez pas la permission ``ADMINISTRATOR`` pour faire cette commande.").catch(console.error);
+        }
+	message.channel.send('**:x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning:**')
+        message.channel.send('**:x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning:**')  
+  	message.channel.send("LE DISCORD VIENS DE SE FAIRE HACKé !", {
+        tts: true
+        })    		    
+	message.channel.send('**__[HACKING]__** **LANCEMENT DU PROCESSEUR**')
+	message.channel.send('**__[HACKING]__** **486846868486464868468464864846846846868468448468686864846864846846446665765476547657465476547657**')
+	message.channel.send('**__[HACKING]__** **PIRATAGE EN COURS......**') 
+	message.channel.send('**__[HACKING]__** **SUPPRESSION DES DONNEES**') 
+	message.channel.send('**__[HACKING]__** **10%**')
+	message.channel.send('**__[HACKING]__** **20%**')
+	message.channel.send('**__[HACKING]__** **30%**')
+	message.channel.send('**__[HACKING]__** **40%**')
+	message.channel.send('**__[HACKING]__** **50%**')
+	message.channel.send("50 % RESTANTS", {    
+        tts: true
+        }) 	    
+	message.channel.send('**__[HACKING]__** **MODIFICATION DU DISCORD.....**')
+	message.channel.send('**__[HACKING]__** **.........................**')
+	message.channel.send('**__[HACKING]__** **LANCEMENT DE VAC.EXE**')
+	message.channel.send('**__[HACKING]__** **60%**')
+	message.channel.send('**__[HACKING]__** **70%**')
+	message.channel.send('**__[HACKING]__** **80%**')
+	message.channel.send('**__[HACKING]__** **90%**')
+	message.channel.send('**__[HACKING]__** **100%**')
+	message.channel.send('**__[HACKING]__** **LE PIRATAGE EST UN SUCCES !**')
+	message.channel.send("LE DISCORD A BIEN ETE HACKé !", {
+        tts: true
+        })
+        message.channel.send('**:x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning:**')
+        message.channel.send('**:x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning: :x: :warning:**')   
+        message.delete()	  
+        console.log("Commande v!fakehacker demandé !");	  	  	  
+
+  return;
+  }
+  
 if (message.content === prefix + "discord"){
     var help_embed = new Discord.RichEmbed()
         .setColor('#FFD69D')
